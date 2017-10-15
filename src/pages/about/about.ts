3688335d0273e7f0b6bcab9ee474976d1e02c6f8
nazik <nmykhalkevych@ukrinsoft.net>
2017-10-15 16:48:11 +0300

    test gestures

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  public press: number = 0;
+  public pan: number = 0;
+  public swipe: number = 0;
+  public tap: number = 0; 
+  public pinch: number = 0; 
+  public rotate: number = 0; 
+  
+
   constructor(public navCtrl: NavController) {
 
   }
-
+  pressEvent(e) {
+    this.press++
+  }
+  panEvent(e) {
+    this.pan++
+  }
+  swipeEvent(e) {
+    this.swipe++
+  }
+  tapEvent(e) {
+    this.tap++
+  }
+  rotateEvent(e) {
+    this.rotate++
+  }
+  pinchEvent(e) {
+    this.pinch++
+  }
 }
